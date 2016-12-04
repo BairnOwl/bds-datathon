708efdd85e04674e11880cf9da3ae105c1a67084
Linda Chang <linda_chang@brown.edu>
2016-12-04 13:19:25 -0500

    added stencil for server

---
+++ server.js
@@ -0,0 +1,25 @@
+/**
+ * Created by bairnowl on 12/4/16.
+ */
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+var engines = require('consolidate');
+app.engine('html', engines.hogan);
+app.set('views', __dirname + '/templates');
+app.use(express.static('public'));
+
+var http = require('http');
+var server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+server.listen(8080, function () {
+    console.log('bds-datathon app listening on port ' + 8080);
+});
+
+
